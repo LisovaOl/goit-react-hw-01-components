fba6698de2233ea30223815f467bb8ecc663a441
LisovaOl <lisova.olenka@gmail.com>
2022-10-11 22:10:36 +0300

    transaction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import userInfo from '../Data/user.json';
 import statInfo from '../Data/data.json';
 import friends from '../Data/friends.json';
-
+import transactions from '../Data/transactions.json'
 
 import UserProfile from './User/User';
 import StatList from './Statistics/StatisticsList';
 import FriendList from './Friends/FriendsList';
+import TransactionHistory from './Transaction/Transaction';
 
 export const App = () => {
   return (
@@ -20,6 +21,7 @@ export const App = () => {
 
       <StatList title="Upload stats" stats={statInfo} />
       <FriendList friends={friends} />
+      <TransactionHistory transactions = {transactions}/>
     </div>
   );
 };
