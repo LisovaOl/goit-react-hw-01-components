16a3fbf84b95ce79fadef8bf0f79af5ea61840ad
LisovaOl <lisova.olenka@gmail.com>
2022-10-12 20:48:52 +0300

    stat

--- src/components/Statistics/StatisticsItem.jsx
+++ src/components/Statistics/StatisticsItem.jsx
@@ -3,23 +3,15 @@ import css from './StatisticsList.module.css';
 
 export default function StatItem({ stats }) {
   return (
-    <li
-      key={stats.id}
-      className={css.item}
-      style={{ backgroundColor: getRandomHexColor() }}
-    >
+    <div>
       <span className={css.label}>{stats.label}</span>
       <span className={css.percentage}>{stats.percentage}%</span>
-    </li>
+    </div>
   );
 }
 StatItem.propTypes = {
   stats: PropTypes.shape({
     label: PropTypes.string,
     percentage: PropTypes.number,
-    id: PropTypes.string.isRequired,
   }),
 };
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
--- src/components/Statistics/StatisticsList.jsx
+++ src/components/Statistics/StatisticsList.jsx
@@ -8,9 +8,26 @@ export default function StatList({ title, stats }) {
       {title && <h2 className={css.title}>{title}</h2>}
       <ul className={css.stat_list}>
         {stats.map(stat => (
-          <StatItem stats={stat} />
+          <li
+            key={stats.id}
+            className={css.item}
+            style={{ backgroundColor: getRandomHexColor() }}
+          >
+            <StatItem stats={stat} />
+          </li>
         ))}
       </ul>
     </section>
   );
 }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+StatList.propTypes = {
+  title: PropTypes.string.isRequired,
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+};
