8ff80ece741224b5eb1140917656af2baf0d5b01
LisovaOl <lisova.olenka@gmail.com>
2022-10-12 20:06:38 +0300

    format

--- src/components/Statistics/StatisticsList.jsx
+++ src/components/Statistics/StatisticsList.jsx
@@ -14,27 +14,3 @@ export default function StatList({ title, stats }) {
     </section>
   );
 }
-
-// StatList.propTypes = {
-//   stats: PropTypes.arrayOf(
-//     PropTypes.shape({
-
-//     })
-//   ),
-// };
-
-//export default function Statistics({ title, stats }) {
-//   return (
-//     <section>
-//       <h2>{title}</h2>
-//       <ul>
-//         {stats.map(stat => (
-//           <li key = {stat.id}>
-//             <span>{stat.label}</span>
-//             <span>{stat.percentage}%</span>
-//           </li>
-//         ))}
-//       </ul>
-//     </section>
-//   );
-// }
