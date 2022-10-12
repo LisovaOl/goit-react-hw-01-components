e34a17f511005de0c47a9bef79494d575038d85c
LisovaOl <lisova.olenka@gmail.com>
2022-10-12 20:37:57 +0300

    friends

--- src/components/Friends/FriendsListItem.jsx
+++ src/components/Friends/FriendsListItem.jsx
@@ -1,10 +1,18 @@
+import { calculateNewValue } from '@testing-library/user-event/dist/utils';
 import PropTypes from 'prop-types';
 import css from './FriendsList.module.css';
 
 export default function FriendItem({ friends }) {
+  // console.log(friends.isOnline);
+
   return (
     <div className={css.friends_item}>
-      <span className={css.status} >{friends.isOnline}</span>
+      <span
+        className={css.status}
+        style={{ backgroundColor: friends.isOnline ? 'green' : 'red' }}
+      >
+        {friends.isOnline}
+      </span>
       <img
         className={css.avatar}
         src={friends.avatar}
